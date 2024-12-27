059eb13b57c7175fa5c33c634ca077877dcdd569
asirialwis <asirichathurika900@gmail.com>
2024-12-27 11:34:13 +0530

    add model class

---
+++ models/Book.cs
@@ -0,0 +1,8 @@
+public class Book
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Author { get; set; }
+    public required string ISBN { get; set; }
+    public DateTime PublicationDate { get; set; }
+}
